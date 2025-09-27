69414d527ca2a244438bb0eeda1f9e5832b86cea
Ola <ponikowska.books@gmail.com>
2025-09-27 23:14:34 +0200

    Add delay load

--- src/content.js
+++ src/content.js
@@ -2,11 +2,10 @@
 
 (async () => {
 
-  const { blockedSites, groups, rules } = await new Promise((resolve) => {
+  const { blockedSites, rules } = await new Promise((resolve) => {
     chrome.storage.sync.get(
       {
         blockedSites: [],
-        groups: [{ id: 0, name: "default" }],
         rules: [
           {
             id: 0,
@@ -46,6 +45,8 @@
       });
   });
 
+  const maxDelay = 0;
+
 
   function applyRules() {
     activeRules.forEach(rule => {
@@ -55,9 +56,9 @@
           blockPage();
           break;
         case 1:
-          // TODO: add delay
+          maxDelay = 5;
           console.log("delay")
-          delayPage(10);
+          
           break;
         case 2:
           // TODO: custom strength
@@ -65,11 +66,68 @@
           break;
       }
     });
+
+    if(maxDelay != 0){
+          (async () => {
+            await delayPage(maxDelay);
+          })();
+    }
+
+
+
   }
 
   applyRules();
 
 })();
+
+async function unlockSite(durationSeconds) {
+  const now = Date.now();
+  const unlockUntil = now + durationSeconds * 1000;
+
+  const { unlockedSites } = await new Promise(resolve => {
+    chrome.storage.sync.get({ unlockedSites: [] }, resolve);
+  });
+
+  const hostname = window.location.hostname;
+  const existingIndex = unlockedSites.findIndex(s => s.hostname === hostname);
+
+  if (existingIndex >= 0) {
+    unlockedSites[existingIndex].unlockUntil = unlockUntil;
+  } else {
+    unlockedSites.push({ hostname, unlockUntil });
+  }
+
+  await chrome.storage.sync.set({ unlockedSites });
+}
+
+
+async function isSiteUnlocked() {
+
+  const hostname = window.location.hostname;
+
+
+  const result = await new Promise(resolve => {
+    chrome.storage.sync.get({ unlockedSites: [] }, resolve);
+  });
+
+  const unlockedSites = result.unlockedSites;
+  const site = unlockedSites.find(site => site.hostname === hostname);
+
+  // site is not unlocked
+  if (!site) return false;
+
+  if (Date.now() > site.unlockUntil) {
+    // it was unlocked, but is no more
+    const newSites = unlockedSites.filter(s => s.hostname !== hostname);
+    await chrome.storage.sync.set({ unlockedSites: newSites });
+    return false;
+  }
+
+  //not on the list
+
+  return true;
+}
 
 function createOverlaySkeleton() {
   const overlay = document.createElement("div");
@@ -103,10 +161,29 @@ function createOverlay(text) {
   message.textContent = text;
 }
 
-function delayPage(init_seconds){
+async function delayPage(init_seconds) {
 
-  console.log("miau")
+  if (await isSiteUnlocked()) return;
+
+  document.documentElement.innerHTML = "";
+  window.stop();
+
+  let liczba = init_seconds;
+  createOverlay(liczba);
+
+  const interval = setInterval(async () => {
+
+    liczba--;
+    createOverlay(liczba);
+
+    if (liczba <= 0) {
+      clearInterval(interval);
+      await unlockSite(1);
+      location.reload();
+    }
+  }, 1000);
 }
+
 
 function blockPage() {
   document.documentElement.innerHTML = "";
